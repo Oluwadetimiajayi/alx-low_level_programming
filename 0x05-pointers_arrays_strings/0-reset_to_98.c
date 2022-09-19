ba4bcc40ca3f2e4c2096a01061b3cc5e32538ad2
Oluwadetimiajayi <oluwadetimiajayi@gmail.com>
2022-09-19 16:36:09 -0700

    trial point

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that takes a pointer to an int as parameter
+ * updates the value it points to to 98
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int *p = &n
+
+	*p = 98
+	
+	printf("%p\n");
+
+	return (0);
+}
