037c1c96020e6ba15d76a066766474028c6b149b
Oluwadetimiajayi <oluwadetimiajayi@gmail.com>
2022-09-20 16:08:19 -0700

    mreere

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse followed by a new line
+ * print_rev - prints a string in reverse, followed by a new line
  * @s: string to be printed
  */
 void print_rev(char *s)
@@ -14,7 +14,7 @@ void print_rev(char *s)
 	{
 		i++;
 	}
-	
+
 	len = i;
 
 	for (j = len - 1; j >= 0; j--)
